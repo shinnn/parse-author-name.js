7a701a4241f3bcab65b299669880d590cabc15bb
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-05-27 16:04:03 +0900

    use ES.next code for testing

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
 'use strict';
 
-var test = require('tape');
+const test = require('tape');
 
 function runTest(description, main) {
-  test(description, function(t) {
+  test(description, t => {
     t.plan(5);
 
     t.equal(main.name, 'parseAuthorName', 'should have a function name.');
@@ -11,28 +11,28 @@ function runTest(description, main) {
     t.equal(main('foo <bar> (baz)'), 'foo', 'should parse author\'s name.');
 
     t.strictEqual(
-      main('<foo>'), null,
+      main('<foo>'),
+      null,
       'should return null when it cannot parse author\'s name.'
     );
 
     t.throws(
-      main.bind(null, true),
+      () => main(true),
       /TypeError.*true.*must be a string/,
       'should throw a type error when it takes a non-string argument.'
     );
 
     t.throws(
-      main.bind(null),
+      () => main(),
       /TypeError.*undefined.*must be a string/,
       'should throw a type error when it takes no arguments.'
     );
   });
 }
 
-runTest('require(\'parse-author-name\')', require('./'));
+runTest('require(\'parse-author-name\')', require('.'));
 
 global.window = {};
-var bowerMain = require('./bower.json').main;
-require('./' + bowerMain);
+require('./' + require('./bower.json').main);
 
 runTest('window.parseAuthorName', window.parseAuthorName);
